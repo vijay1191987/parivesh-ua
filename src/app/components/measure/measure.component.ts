6aee6a4e9ffd6da3bc09d8a674a688fc6269a9d0
raushan.raman <raushan.raman@aurionpro.com>
2023-02-23 18:00:44 +0530

    Complete Elevation Profile

--- src/app/components/measure/measure.component.ts
+++ src/app/components/measure/measure.component.ts
@@ -1,94 +1,72 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { DragableService } from 'src/app/services/dragable.service';
 import { loadModules } from 'esri-loader';
-
-
-
-let elevationProfile: any = null;
 
 @Component({
   selector: 'app-measure',
   templateUrl: './measure.component.html',
   styleUrls: ['./measure.component.css']
 })
-export class MeasureComponent implements OnInit {
+export class MeasureComponent {
   @Input() MapData: any = {};
   PariveshGIS: any = {};
-  isModalOpen: boolean = false;
   ESRIObject: object = {};
   ESRIObj_: any = null;
-  xyShow: boolean = false;
-  elevationShow:boolean = false;
-
-  
+  elevationProfile: any = null;
+  elevationShow: boolean = false;
 
   constructor(private dragable: DragableService) { }
-  async ngOnInit(){   
-    
-  }
-   
-  async elevationIcon(idName: any)
-  {
+
+  async elevationIcon(idName: any) {
     this.elevationShow = !this.elevationShow;
     this.dragable.registerDragElement(idName);
+    //Map Object
     const t: any = this.MapData;
-    if (t.ESRIObj_.hasOwnProperty("ESRIObj_")) {
+    if (t.ESRIObj_.hasOwnProperty("ESRIObj_"))
       this.PariveshGIS = await t.ESRIObj_.ESRIObj_;
+    else
+      this.PariveshGIS = await t.ESRIObj_;   
+
+    if (this.elevationShow) {
+      this.elevationProfile = null;
+      const rootElem: any = document.getElementById('elevation');
+      const d: any = document.createElement('div');
+      d.className = 'tool-popup-inner';
+      rootElem.appendChild(d)
+      const [ElevationProfile, ElevationLayer] = await loadModules(["esri/widgets/ElevationProfile", "esri/layers/ElevationLayer"]);
+      const worldElevation = new ElevationLayer({
+        url: "https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer",
+        id: "elevationlayer"
+      });
+      this.PariveshGIS.ArcMap.ground.layers.add(worldElevation);
+      
+      this.elevationProfile = new ElevationProfile({
+        view: this.PariveshGIS.ArcView,
+        profiles: [
+          {
+            type: "ground" // first profile line samples the ground elevation
+          },
+          {
+            type: "view" // second profile samples the view and shows building profiles
+          },
+
+        ],
+        visibleElements: {
+          legend: true,
+          chart: true,
+          clearButton: true,
+          settingsButton: true,
+          sketchButton: true,
+          selectButton: false,
+          uniformChartScalingToggle: true
+        },
+        container: d // runtime div created
+      });
     }
     else {
-      this.PariveshGIS = await t.ESRIObj_;
-      console.log('measure-component: ', this.PariveshGIS);
-    } 
-    
-    const [ESRIMap,ElevationProfile,ElevationLayer,Ground] = await loadModules(['esri/Map',"esri/widgets/ElevationProfile","esri/layers/ElevationLayer","esri/Ground"]);
-
-    const worldElevation = new ElevationLayer({
-      url: "https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer",
-      id: "elevationlayer"
-    });
-    // this.PariveshGIS.ArcMap = new ESRIMap({
-    //   basemap: null
-    // });
-
-    this.PariveshGIS.ArcMap.ground.layers.add(worldElevation);                    
-    
-    
-    elevationProfile = new ElevationProfile({
-      view: this.PariveshGIS.ArcView,
-      profiles: [
-        {
-          type: "ground" // first profile line samples the ground elevation
-        },
-        {
-          type: "view" // second profile samples the view and shows building profiles
-        }
-      ],
-      visibleElements: {
-        selectButton: false
+      if (this.elevationProfile != undefined) {
+        this.elevationProfile.destroy(); // to destroy the elevation profile
       }
-    });
-    
-    
-  } 
-  async elevProfile(){  
-    //const elevationbtn = document.getElementById("elevation");  
-    var search = document.getElementsByClassName("esri-elevation-profile").length;
-      if (search > 0) {
-        this.closeElavation();
-      }
-      else {
-        this.elevation1();
-      }
-  }
-  
-  async elevation1() {
-    
-    this.PariveshGIS.ArcView.ui.add(elevationProfile, 'bottom-right');
-  }
-  closeElavation() {
-    while (document.getElementsByClassName('esri-elevation-profile')[0]) {
-      document.getElementsByClassName('esri-elevation-profile')[0].remove();
     }
-
-}
+  }
 }
